0deb1cf9b51937ff7dd60bad654263b6dd7be0b1
rrymaszewski <radoslaw.rymaszewski@gmail.com>
2010-01-14 22:55:13 +0000

    removed tmp directory

--- src/main/java/pl/pronux/sokker/ui/plugins/ViewCalendar.java
+++ src/main/java/pl/pronux/sokker/ui/plugins/ViewCalendar.java
@@ -74,7 +74,6 @@ public class ViewCalendar implements IPlugin {
 
 	private DecimalFormat doubleDigitFormat;
 
-
 	public void clear() {
 
 	}
@@ -108,9 +107,9 @@ public class ViewCalendar implements IPlugin {
 	}
 
 	private void initComposite(Composite composite) {
-		
+
 		Composite composite2;
-		
+
 		FormData formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.top = new FormAttachment(0, 0);
@@ -120,33 +119,29 @@ public class ViewCalendar implements IPlugin {
 		Composite compositeView = new Composite(composite, SWT.BORDER);
 		compositeView.setLayoutData(formData);
 		compositeView.setLayout(new FormLayout());
-		
+
 		compositeView.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 
 		formData = new FormData();
-		// formData.height = 150;
 		formData.width = 250;
 		formData.left = new FormAttachment(50, -formData.width / 2);
 		formData.top = new FormAttachment(0, 5);
-		
+
 		Composite groupCalendar = new Composite(compositeView, SWT.NONE);
 		groupCalendar.setLayoutData(formData);
 		groupCalendar.setLayout(new FormLayout());
-//		groupCalendar.setText(langResource.getString("calendar"));
-		
-		formData = new FormData();
-		// formData.height = 150;
-		formData.left = new FormAttachment(0,5);
-		formData.top = new FormAttachment(0, 5);
-		formData.bottom = new FormAttachment(100,-5);
-		formData.right = new FormAttachment(100,-5);
 
-//		calendar = new SWTCalendar(groupCalendar, SWTCalendar.RED_SUNDAY);
+		formData = new FormData();
+		formData.left = new FormAttachment(0, 5);
+		formData.top = new FormAttachment(0, 5);
+		formData.bottom = new FormAttachment(100, -5);
+		formData.right = new FormAttachment(100, -5);
+
 		calendar = new DateTime(groupCalendar, SWT.CALENDAR);
 		calendar.setLayoutData(formData);
 		calendar.setFont(ConfigBean.getFontMain());
 		calendar.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
-		
+
 		calendar.addListener(SWT.Selection, new Listener() {
 
 			public void handleEvent(Event event) {
@@ -154,15 +149,8 @@ public class ViewCalendar implements IPlugin {
 				cal.set(calendar.getYear(), calendar.getMonth(), calendar.getDay());
 				getDayEvents(cal);
 			}
-			
+
 		});
-//		calendar.addSWTCalendarListener(new SWTCalendarListener() {
-//
-//			public void dateChanged(SWTCalendarEvent event) {
-//				getDayEvents(calendar.getCalendar());
-//			}
-//
-//		});
 
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 5);
@@ -179,17 +167,16 @@ public class ViewCalendar implements IPlugin {
 		formData.left = new FormAttachment(0, 5);
 		formData.right = new FormAttachment(50, -5);
 		formData.top = new FormAttachment(0, 5);
-		formData.bottom = new FormAttachment(100,0);
+		formData.bottom = new FormAttachment(100, 0);
 
 		ExpandBar bar1 = new ExpandBar(group, SWT.V_SCROLL);
 		bar1.setLayoutData(formData);
 		bar1.setSpacing(5);
-		
-		
+
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
@@ -202,18 +189,18 @@ public class ViewCalendar implements IPlugin {
 		text1.setLinesVisible(false);
 		text1.setHeaderVisible(false);
 		text1.setFont(ConfigBean.getFontTable());
-		
+
 		new TableColumn(text1, SWT.LEFT);
-		
+
 		textDayEvery = new ExpandItem(bar1, SWT.NONE, 0);
-		textDayEvery.setControl(composite2);		
+		textDayEvery.setControl(composite2);
 		textDayEvery.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDayEvery.setExpanded(true);
-		
+
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
@@ -223,29 +210,28 @@ public class ViewCalendar implements IPlugin {
 		text2 = new Table(composite2, SWT.LEFT);
 		text2.setBackground(composite2.getBackground());
 		text2.setLayoutData(formData);
-		
+
 		text2.setLinesVisible(false);
 		text2.setHeaderVisible(false);
 		text2.setFont(ConfigBean.getFontTable());
-		
+
 		new TableColumn(text2, SWT.LEFT);
 
 		textDaySaturday = new ExpandItem(bar1, SWT.NONE, 1);
-		textDaySaturday.setControl(composite2);		
+		textDaySaturday.setControl(composite2);
 		textDaySaturday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDaySaturday.setExpanded(true);
-		
 
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
 		formData.top = new FormAttachment(0, 0);
 		formData.bottom = new FormAttachment(100, 0);
-		
+
 		text3 = new Table(composite2, SWT.READ_ONLY);
 		text3.setBackground(composite2.getBackground());
 		text3.setLayoutData(formData);
@@ -253,157 +239,155 @@ public class ViewCalendar implements IPlugin {
 
 		text3.setLinesVisible(false);
 		text3.setHeaderVisible(false);
-		
+
 		new TableColumn(text3, SWT.LEFT);
-		
+
 		textDaySunday = new ExpandItem(bar1, SWT.NONE, 2);
-		textDaySunday.setControl(composite2);		
+		textDaySunday.setControl(composite2);
 		textDaySunday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDaySunday.setExpanded(true);
-
 
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
 		formData.top = new FormAttachment(0, 0);
 		formData.bottom = new FormAttachment(100, 0);
-		
-		text4 = new Table(composite2, SWT.READ_ONLY );
+
+		text4 = new Table(composite2, SWT.READ_ONLY);
 		text4.setBackground(composite2.getBackground());
 		text4.setLayoutData(formData);
 
 		text4.setLinesVisible(false);
 		text4.setHeaderVisible(false);
 		text4.setFont(ConfigBean.getFontTable());
-		
+
 		new TableColumn(text4, SWT.LEFT);
-		
+
 		textDayMonday = new ExpandItem(bar1, SWT.NONE, 3);
-		textDayMonday.setControl(composite2);		
+		textDayMonday.setControl(composite2);
 		textDayMonday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDayMonday.setExpanded(true);
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(50, 5);
 		formData.right = new FormAttachment(100, -5);
 		formData.top = new FormAttachment(0, 5);
-		formData.bottom = new FormAttachment(100,0);
-		
+		formData.bottom = new FormAttachment(100, 0);
+
 		bar1 = new ExpandBar(group, SWT.V_SCROLL);
 		bar1.setLayoutData(formData);
 		bar1.setSpacing(5);
-		
-		
+
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
 		formData.top = new FormAttachment(0, 0);
 		formData.bottom = new FormAttachment(100, 0);
 
-		text5 = new Table(composite2,  SWT.READ_ONLY );
+		text5 = new Table(composite2, SWT.READ_ONLY);
 		text5.setBackground(composite2.getBackground());
 		text5.setLayoutData(formData);
 
 		text5.setLinesVisible(false);
 		text5.setHeaderVisible(false);
 		text5.setFont(ConfigBean.getFontTable());
-		
+
 		new TableColumn(text5, SWT.LEFT);
-		
+
 		textDayTuesday = new ExpandItem(bar1, SWT.NONE, 0);
-		textDayTuesday.setControl(composite2);		
+		textDayTuesday.setControl(composite2);
 		textDayTuesday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDayTuesday.setExpanded(true);
-		
+
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
 		formData.top = new FormAttachment(0, 0);
 		formData.bottom = new FormAttachment(100, 0);
 
-		text6 = new Table(composite2, SWT.READ_ONLY );
+		text6 = new Table(composite2, SWT.READ_ONLY);
 		text6.setBackground(composite2.getBackground());
 		text6.setLayoutData(formData);
 		text6.setFont(ConfigBean.getFontTable());
 
 		text6.setLinesVisible(false);
 		text6.setHeaderVisible(false);
-		
+
 		new TableColumn(text6, SWT.LEFT);
-		
+
 		textDayWednesday = new ExpandItem(bar1, SWT.NONE, 1);
-		textDayWednesday.setControl(composite2);		
+		textDayWednesday.setControl(composite2);
 		textDayWednesday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDayWednesday.setExpanded(true);
 
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
 		formData.top = new FormAttachment(0, 0);
 		formData.bottom = new FormAttachment(100, 0);
-		
-		text7 = new Table(composite2, SWT.READ_ONLY );
+
+		text7 = new Table(composite2, SWT.READ_ONLY);
 		text7.setBackground(composite2.getBackground());
 		text7.setLayoutData(formData);
 
 		text7.setLinesVisible(false);
 		text7.setHeaderVisible(false);
 		text7.setFont(ConfigBean.getFontTable());
-		
+
 		new TableColumn(text7, SWT.LEFT);
-		
+
 		textDayThursday = new ExpandItem(bar1, SWT.NONE, 2);
-		textDayThursday.setControl(composite2);		
+		textDayThursday.setControl(composite2);
 		textDayThursday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDayThursday.setExpanded(true);
-
 
 		composite2 = new Composite(bar1, SWT.NONE);
 		composite2.setBackground(composite.getDisplay().getSystemColor(SWT.COLOR_WHITE));
 		composite2.setLayout(new FormLayout());
-		
+
 		formData = new FormData();
 		formData.left = new FormAttachment(0, 0);
 		formData.right = new FormAttachment(100, 0);
 		formData.top = new FormAttachment(0, 0);
 		formData.bottom = new FormAttachment(100, 0);
-		
-		text8 = new Table(composite2, SWT.READ_ONLY );
+
+		text8 = new Table(composite2, SWT.READ_ONLY);
 		text8.setBackground(composite2.getBackground());
 		text8.setLayoutData(formData);
 		text8.setFont(ConfigBean.getFontTable());
 
 		text8.setLinesVisible(false);
 		text8.setHeaderVisible(false);
-		
+
 		new TableColumn(text8, SWT.LEFT);
-		
+
 		textDayFriday = new ExpandItem(bar1, SWT.NONE, 3);
-		textDayFriday.setControl(composite2);		
+		textDayFriday.setControl(composite2);
 		textDayFriday.setHeight(composite2.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		textDayFriday.setExpanded(true);
-		
+
 		getDayEvents(Calendar.getInstance());
 	}
 
 	private String formatDate(Calendar cal) {
-		return cal.get(Calendar.YEAR) + "-" + doubleDigitFormat.format((cal.get(Calendar.MONTH) + 1)) + "-" + doubleDigitFormat.format(cal.get(Calendar.DAY_OF_MONTH));
+		return cal.get(Calendar.YEAR) + "-" + doubleDigitFormat.format((cal.get(Calendar.MONTH) + 1)) + "-"
+			   + doubleDigitFormat.format(cal.get(Calendar.DAY_OF_MONTH));
 	}
 
 	private void getDayEvents(Calendar calendar) {
@@ -413,19 +397,20 @@ public class ViewCalendar implements IPlugin {
 
 		int diff = 0;
 		Season season = new Season(calendar.getTimeInMillis());
-		group.setText(Messages.getString("training.season") + " " + season.getSeasonNumber() + " " + Messages.getString("training.week") + " " + season.getSeasonWeek());
+		group.setText(Messages.getString("training.season") + " " + season.getSeasonNumber() + " " + Messages.getString("training.week") + " "
+					  + season.getSeasonWeek());
 
 		textDayEvery.setText(Messages.getString("calendar.day.0"));
-		
+
 		text1.removeAll();
-		
+
 		new TableItem(text1, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.1"));
 		new TableItem(text1, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.2"));
 		new TableItem(text1, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.3"));
 		new TableItem(text1, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.4"));
-		
+
 		text1.getColumn(0).pack();
-		
+
 		if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
 			diff = 0;
 		} else {
@@ -435,10 +420,9 @@ public class ViewCalendar implements IPlugin {
 		cal.setTimeInMillis(calendar.getTimeInMillis() + diff * (24 * 60 * 60 * 1000));
 
 		textDaySaturday.setText(Messages.getString("calendar.day." + Calendar.SATURDAY) + " " + "(" + formatDate(cal) + ")");
-		
+
 		text2.removeAll();
-		
-	
+
 		new TableItem(text2, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.29"));
 		new TableItem(text2, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.7"));
 		new TableItem(text2, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.28"));
@@ -447,7 +431,7 @@ public class ViewCalendar implements IPlugin {
 
 		text2.getColumn(0).pack();
 		text3.removeAll();
-		
+
 		if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
 			diff = Calendar.SUNDAY;
 		} else {
@@ -456,7 +440,7 @@ public class ViewCalendar implements IPlugin {
 		cal.setTimeInMillis(calendar.getTimeInMillis() + diff * (24 * 60 * 60 * 1000));
 
 		textDaySunday.setText(Messages.getString("calendar.day." + Calendar.SUNDAY) + " " + "(" + formatDate(cal) + ")");
-		
+
 		if (season.getSeasonWeek() == 0) {
 			new TableItem(text3, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.20"));
 		} else if (season.getSeasonWeek() == 15) {
@@ -470,7 +454,6 @@ public class ViewCalendar implements IPlugin {
 		text3.getColumn(0).pack();
 		text4.removeAll();
 
-
 		if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
 			diff = Calendar.MONDAY;
 		} else {
@@ -480,7 +463,6 @@ public class ViewCalendar implements IPlugin {
 
 		textDayMonday.setText(Messages.getString("calendar.day." + Calendar.MONDAY) + " " + "(" + formatDate(cal) + ")");
 
-		
 		if (season.getSeasonWeek() == 0) {
 			new TableItem(text4, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.24"));
 		}
@@ -493,31 +475,30 @@ public class ViewCalendar implements IPlugin {
 		text4.getColumn(0).pack();
 		text5.removeAll();
 
-
 		if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
 			diff = Calendar.TUESDAY;
 		} else {
 			diff = Calendar.TUESDAY - calendar.get(Calendar.DAY_OF_WEEK);
 		}
-		
+
 		cal.setTimeInMillis(calendar.getTimeInMillis() + diff * (24 * 60 * 60 * 1000));
 
 		textDayTuesday.setText(Messages.getString("calendar.day." + Calendar.TUESDAY) + " " + "(" + formatDate(cal) + ")");
-		
+
 		new TableItem(text5, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.26"));
 
 		text5.getColumn(0).pack();
 		text6.removeAll();
 
 		if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
-			diff = Calendar.WEDNESDAY ;
+			diff = Calendar.WEDNESDAY;
 		} else {
 			diff = Calendar.WEDNESDAY - calendar.get(Calendar.DAY_OF_WEEK);
 		}
 		cal.setTimeInMillis(calendar.getTimeInMillis() + diff * (24 * 60 * 60 * 1000));
 
 		textDayWednesday.setText(Messages.getString("calendar.day." + Calendar.WEDNESDAY) + " " + "(" + formatDate(cal) + ")");
-		
+
 		new TableItem(text6, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.14"));
 		new TableItem(text6, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.22"));
 
@@ -532,7 +513,7 @@ public class ViewCalendar implements IPlugin {
 		cal.setTimeInMillis(calendar.getTimeInMillis() + diff * (24 * 60 * 60 * 1000));
 
 		textDayThursday.setText(Messages.getString("calendar.day." + Calendar.THURSDAY) + " " + "(" + formatDate(cal) + ")");
-		
+
 		new TableItem(text7, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.12"));
 		new TableItem(text7, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.15"));
 		new TableItem(text7, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.16"));
@@ -550,7 +531,7 @@ public class ViewCalendar implements IPlugin {
 		cal.setTimeInMillis(calendar.getTimeInMillis() + diff * (24 * 60 * 60 * 1000));
 
 		textDayFriday.setText(Messages.getString("calendar.day." + Calendar.FRIDAY) + " " + "(" + formatDate(cal) + ")");
-		
+
 		if (season.getSeasonWeek() == 15) {
 			new TableItem(text8, SWT.NONE).setText("- " + Messages.getString("calendar.day.event.19"));
 		} else {
@@ -559,7 +540,6 @@ public class ViewCalendar implements IPlugin {
 
 		text8.getColumn(0).pack();
 
-
 		text2.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_BLACK));
 		text3.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_BLACK));
 		text4.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_BLACK));
@@ -567,7 +547,6 @@ public class ViewCalendar implements IPlugin {
 		text6.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_BLACK));
 		text7.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_BLACK));
 		text8.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_BLACK));
-
 
 		switch (calendar.get(Calendar.DAY_OF_WEEK)) {
 		case Calendar.SATURDAY:
@@ -598,14 +577,14 @@ public class ViewCalendar implements IPlugin {
 			text8.setForeground(composite.getDisplay().getSystemColor(SWT.COLOR_RED));
 			textDayFriday.setExpanded(true);
 			break;
-			
+
 		}
 
 		if (Cache.getNotes() != null) {
-			
+
 			for (Note note : Cache.getNotes()) {
 				if (note.getAlertDate() != null) {
-					if(note.getAlertDate() != null) {
+					if (note.getAlertDate() != null) {
 						cal2.setTimeInMillis(note.getAlertDate().getMillis());
 
 						if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
@@ -618,7 +597,7 @@ public class ViewCalendar implements IPlugin {
 						cal.set(Calendar.MINUTE, 0);
 						cal.set(Calendar.SECOND, 0);
 						cal.set(Calendar.MILLISECOND, 0);
-						
+
 						if (Calendar.SATURDAY == calendar.get(Calendar.DAY_OF_WEEK)) {
 							diff = Calendar.FRIDAY;
 						} else {
@@ -659,7 +638,7 @@ public class ViewCalendar implements IPlugin {
 				}
 			}
 		}
-		
+
 		text1.pack();
 		textDayEvery.setHeight(text1.getParent().computeSize(SWT.DEFAULT, SWT.DEFAULT).y + 5);
 		text2.pack();
@@ -680,7 +659,6 @@ public class ViewCalendar implements IPlugin {
 	}
 
 	public void setSettings(SokkerViewerSettings sokkerViewerSettings) {
-
 	}
 
 	public void setTreeItem(TreeItem treeItem) {
@@ -691,22 +669,14 @@ public class ViewCalendar implements IPlugin {
 
 	public void set() {
 		getDayEvents(Calendar.getInstance());
-
 	}
 
 	public void dispose() {
-		
 	}
 
 	public void setSvBean(SvBean svBean) {
-		
 	}
 
 	public void reload() {
-		// TODO Auto-generated method stub
-		
 	}
-
-
-
 }
