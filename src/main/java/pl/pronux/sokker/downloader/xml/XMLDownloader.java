88f877cf92b1e4aed1a0939c9933b66483d7156a
Radosław Rymaszewski <radoslaw.rymaszewski@gmail.com>
2013-05-12 01:47:22 +0200

    change url of sokker xml files

--- src/main/java/pl/pronux/sokker/downloader/xml/XMLDownloader.java
+++ src/main/java/pl/pronux/sokker/downloader/xml/XMLDownloader.java
@@ -6,75 +6,77 @@ import pl.pronux.sokker.downloader.SokkerAuthentication;
 
 public class XMLDownloader extends SokkerAuthentication {
 
+	private final static String SOKKER_URL = "http://online.sokker.org/xml/";
+	
 	public XMLDownloader() {
 	}
 
 	public String getTrainers() throws IOException {
-		return getXML("http://217.17.40.90/xml/trainers.xml"); 
+		return getXML(SOKKER_URL + "trainers.xml"); 
 	}
 
 	public String getCountries() throws IOException {
-		return getXML("http://217.17.40.90/xml/countries.xml"); 
+		return getXML(SOKKER_URL + "countries.xml"); 
 	}
 
 	public String getCountry(String countryID) throws IOException  {
-		return getXML("http://217.17.40.90/xml/country-" + countryID + ".xml");  
+		return getXML(SOKKER_URL + "country-" + countryID + ".xml");  
 	}
 
 	public String getJuniors() throws IOException  {
-		return getXML("http://217.17.40.90/xml/juniors.xml"); 
+		return getXML(SOKKER_URL + "juniors.xml"); 
 	}
 
 	public String getLeague(String leagueID) throws IOException  {
-		return getXML("http://217.17.40.90/xml/league-" + leagueID + ".xml");  
+		return getXML(SOKKER_URL + "league-" + leagueID + ".xml");  
 	}
 
 	public String getLeague(String countryID, String division, String number) throws IOException  {
-		return getXML("http://217.17.40.90/xml/league-" + countryID + "-" + division + "-" + number + ".xml");    
+		return getXML(SOKKER_URL + "league-" + countryID + "-" + division + "-" + number + ".xml");    
 	}
 
 	public String getMatch(String matchID) throws IOException {
-		return getXML("http://217.17.40.90/xml/match-" + matchID + ".xml");  
+		return getXML(SOKKER_URL + "match-" + matchID + ".xml");  
 	}
 
 	public String getLeagueMatches(String leagueID, String round) throws IOException  {
-		return getXML("http://217.17.40.90/xml/matches-league-" + leagueID + "-" + round + ".xml");   
+		return getXML(SOKKER_URL + "matches-league-" + leagueID + "-" + round + ".xml");   
 	}
 
 	public String getMatchesTeam(String teamID) throws IOException  {
-		return getXML("http://217.17.40.90/xml/matches-team-" + teamID + ".xml");  
+		return getXML(SOKKER_URL + "matches-team-" + teamID + ".xml");  
 	}
 
 	public String getPlayer(String playerID) throws IOException {
-		return getXML("http://217.17.40.90/xml/player-" + playerID + ".xml");  
+		return getXML(SOKKER_URL + "player-" + playerID + ".xml");  
 	}
 
 	public String getPlayers(String teamID) throws IOException  {
-		return getXML("http://217.17.40.90/xml/players-" + teamID + ".xml");  
+		return getXML(SOKKER_URL + "players-" + teamID + ".xml");  
 	}
 
 	public String getRegion(String regionID) throws IOException {
-		return getXML("http://217.17.40.90/xml/region-" + regionID + ".xml");  
+		return getXML(SOKKER_URL + "region-" + regionID + ".xml");  
 	}
 
 	public String getRegions(String countryID) throws IOException{
-		return getXML("http://217.17.40.90/xml/regions-" + countryID + ".xml");  
+		return getXML(SOKKER_URL + "regions-" + countryID + ".xml");  
 	}
 
 	public String getReports() throws IOException {
-		return getXML("http://217.17.40.90/xml/reports.xml"); 
+		return getXML(SOKKER_URL + "reports.xml"); 
 	}
 
 	public String getTeam(String teamID) throws IOException {
-		return getXML("http://217.17.40.90/xml/team-" + teamID + ".xml");  
+		return getXML(SOKKER_URL + "team-" + teamID + ".xml");  
 	}
 
 	public String getTransfers() throws IOException {
-		return getXML("http://217.17.40.90/xml/transfers.xml"); 
+		return getXML(SOKKER_URL + "transfers.xml"); 
 	}
 
 	public String getVars() throws IOException {
-		return getXML("http://217.17.40.90/xml/vars.xml"); 
+		return getXML(SOKKER_URL + "vars.xml"); 
 	}
 
 }
